4b83590f687d0d10642380d5cc68b6b1dd368f1c
Mika Repo <mikeorap@gmail.com>
2023-04-16 14:09:32 +0300

    Osa 2 tehtävät 12-17

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personsService from './services/persons'
 
 const Filter = (props) => {
   return(
@@ -28,8 +28,23 @@ const PersonForm = (props) => {
 const Persons = (props) => {
   return(
     <div>
-      {props.persons.filter(nimi => nimi.name.toLowerCase().includes(props.filterName.toLowerCase())).map(filteredName =>
-      <p key={filteredName.name}>{filteredName.name} {filteredName.number}</p>)}
+      {props.persons.filter(nimi => nimi.name?.toLowerCase().includes(props.filterName?.toLowerCase())).map((filteredName, index) =>
+      <div key={filteredName.name}>
+        <p>{filteredName.name} {filteredName.number}</p>
+        <button onClick={() => props.handleClick(filteredName.id)}>Delete</button> 
+      </div>)}
+    </div>
+  )
+}
+
+const Notification = (props) => {
+  if (props.message === null) {
+    return null
+  }
+
+  return (
+    <div className={props.class}>
+      {props.message}
     </div>
   )
 }
@@ -39,11 +54,13 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterName] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [classOfMessege, setClassOfMessege] = useState('success')
 
   useEffect(() => {
     console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
+    personsService
+      .getAll()
       .then(response => {
         console.log('promise fulfilled')
         setPersons(response.data)
@@ -76,27 +93,87 @@ const App = () => {
     console.log('usedName', usedName)
 
     if (usedName) {
-      window.alert(`${newName} is already added to phonebook`)
+      const allPersons = [...persons]
+      const updateName = allPersons.find(nimi => nimi.name === newName)
+      const updateNameIndex = allPersons.findIndex(nimi => nimi.id === updateName.id)
+      updateName.number = newNumber
+      if (window.confirm('Name is already added to phonebook, replace the old number with new one?')) {
+        personsService
+        .update(updateName.id, updateName)
+        .then(response => {
+          console.log('updated number', response.data)
+          allPersons[updateNameIndex] = response.data
+          setErrorMessage(
+            `Updated '${response.data.name}'`
+          )
+          setClassOfMessege('success')
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000)
+          setPersons(allPersons)
+        })
+        .catch(error => {
+          setErrorMessage(
+            `Information of '${updateName.name}' has already been removed from server`
+          )
+          setClassOfMessege('error')
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000)
+        })
+      }
     } else {
       const nameObject = {
         name: newName,
         number: newNumber,
       }
-    
-      setPersons(persons.concat(nameObject))
-      setNewName('')
-      setNewNumber('')
+      personsService
+      .create(nameObject)
+      .then(response => {
+        console.log('newName to json', response.data)
+        setErrorMessage(
+          `Added '${newName}'`
+        )
+        setClassOfMessege('success')
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      })
+    }
+  }
+
+  const handleDelete = (id) => {
+    console.log('id on:', id)
+    const filteredNames = persons.filter(nimi => nimi.id !== id)
+    if (window.confirm('Do you really want to delete the name?')) {
+      personsService
+      .deleteName(id)
+      .then(response => {
+        console.log('deleted name', response.data)
+        setErrorMessage(
+          `Deleted the name`
+        )
+        setClassOfMessege('success')
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setPersons(filteredNames)
+      })
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage} class={classOfMessege}/>
       <Filter name={filterName} handleFilter={handleFilterName}/>
       <h2>Add new name</h2>
       <PersonForm addNewName={addName} name={newName} handleName={handleAddingName} number={newNumber} handleNumber={handleAddingNumber}/>
       <h2>Numbers</h2>
-      <Persons persons={persons} filterName={filterName}/>
+      <Persons persons={persons} filterName={filterName} handleClick={handleDelete}/>
     </div>
   )
 
